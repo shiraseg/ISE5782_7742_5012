87c83e2b6e6a25774f22b16eee6896b2d221e2ce
שירה סגל <shisegal@g.jct.ac.il>
2022-03-09 19:44:31 +0200

    tests for Point

--- src/Main.java
+++ src/Main.java
@@ -26,18 +26,20 @@ public final class Main {
         Vector v2 = new Vector(-2, -4, -6);
         Vector v3 = new Vector(0, 3, -2);
 
+/**
         // test length..
         if (!isZero(v1.lengthSquared() - 14))
             out.println("ERROR: lengthSquared() wrong value");
         if (!isZero(new Vector(0, 3, 4).length() - 5))
             out.println("ERROR: length() wrong value");
-/**
+
+
         // test Dot-Product
         if (!isZero(v1.dotProduct(v3)))
             out.println("ERROR: dotProduct() for orthogonal vectors is not zero");
         if (!isZero(v1.dotProduct(v2) + 28))
             out.println("ERROR: dotProduct() wrong value");
- */
+
 
         // test Cross-Product
         try { // test zero vector
@@ -51,7 +53,6 @@ public final class Main {
         if (!isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)))
             out.println("ERROR: crossProduct() result is not orthogonal to its operands");
 
-/**
         // test vector normalization vs vector length and cross-product
         Vector v = new Vector(1, 2, 3);
         Vector u = v.normalize();
@@ -64,7 +65,7 @@ public final class Main {
         }
         if (v.dotProduct(u) < 0)
             out.println("ERROR: the normalized vector is opposite to the original one");
-*/
+
 
         // Test operations with points and vectors
         Point p1 = new Point(1, 2, 3);
@@ -72,7 +73,7 @@ public final class Main {
             out.println("ERROR: Point + Vector does not work correctly");
         if (!new Vector(1, 1, 1).equals(new Point(2, 3, 4).subtract(p1)))
             out.println("ERROR: Point - Point does not work correctly");
-
+ */
         out.println("If there were no any other outputs - all tests succeeded!");
     }
 }
--- src/unittests/primitives/PointTest.java
+++ src/unittests/primitives/PointTest.java
@@ -1,6 +1,8 @@
 package unittests.primitives;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,10 +10,14 @@ class PointTest {
 
     @Test
     void add() {
+        Point p1 = new Point(1, 2, 3);
+        assertEquals(p1.add(new Vector(-1, -2, -3)),new Point(0, 0, 0),"ERROR: Point + Vector does not work correctly");
     }
 
     @Test
     void subtract() {
+        Point p1 = new Point(1, 2, 3);
+        assertEquals(new Vector(1, 1, 1),new Point(2, 3, 4).subtract(p1),"ERROR: Point - Point does not work correctly");
     }
 
     @Test
@@ -19,6 +25,8 @@ class PointTest {
     }
 
     @Test
-    void distance() {
+    void distance()
+    {
+
     }
 }
