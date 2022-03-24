c9b0eb47466f414690b8582da74e2d3c49eebce2
Noam Sivan <noamorashauly@gmail.com>
2022-03-24 15:09:52 +0200

    working on camera

--- src/Renderer/Camera.java
+++ src/Renderer/Camera.java
@@ -1,12 +1,12 @@
 package Renderer;
 
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
 
 
 
-public class Camera
-{
+public class Camera {
     private Point loc;
     private Vector vTo;
     private Vector vUp;
@@ -15,26 +15,40 @@ public class Camera
     private double height;
     private double distance;
 
-    public double getWidth()
-    {
+    public double getWidth() {
         return width;
     }
 
-    public double getHeight()
-    {
+    public double getHeight() {
         return height;
     }
 
-    public double getDistance()
-    {
+    public double getDistance() {
         return distance;
     }
 
-    public Camera(Vector vTo, Vector vUp, int i, int j)
-    {
-        if(vTo.dotProduct(vUp)==0)  throw new IllegalArgumentException("ERROR! two vectors arent orthogonal!");
+    public Camera(Vector vTo, Vector vUp, int i, int j)         /*constructor*/ {
+        if (vTo.dotProduct(vUp) == 0) throw new IllegalArgumentException("ERROR! two vectors arent orthogonal!");
         this.vTo = vTo.normalize();
         this.vUp = vUp.normalize();
-        this.vRight=vTo.crossProduct(vUp).normalize();
+        this.vRight = vTo.crossProduct(vUp).normalize();
+    }
+
+
+    /*builder pattern methods:*/
+    public Camera setVPSize(double width, double height) {
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+
+    public Camera setVPDistance(double distance) {
+        this.distance = distance;
+        return this;
+    }
+
+    public Ray constructRay(int nX, int nY, int j, int i)
+    {
+        return null;
     }
 }
