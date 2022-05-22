07affdf665fec74de0c080217f27d80704a46f70
שירה סגל <shisegal@g.jct.ac.il>
2022-05-22 19:18:57 +0300

    finish with targil 7!!!

---
+++ src/unittests/firstImage.java
@@ -0,0 +1,109 @@
+package unittests;
+
+import Lighting.SpotLight;
+import Renderer.Camera;
+import Renderer.ImageWriter;
+import Renderer.RayTracerBasic;
+import geometries.Geometry;
+import geometries.Plane;
+import geometries.Sphere;
+import geometries.Triangle;
+import org.junit.jupiter.api.Test;
+import primitives.*;
+import scene.Scene;
+
+/**
+ * this is our first image
+ * we create a sunrise scene
+ * including sea, birds and the sun
+ * using plane, sphere and triangles
+ */
+public class firstImage {
+    @Test
+    public void test()
+    {
+        //creating the scene
+        Scene scene1 = new Scene.SceneBuilder("Test scene").build();
+        Camera camera1 = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVPSize(150, 150) //
+                .setVPDistance(1000);
+
+        //creating the elements
+
+        //SKY (we chose to create it from sphere  to give the feeling of the sky
+        Geometry sky = new Sphere(new Point(0,0,-1000),1000)
+                .setEmission(new Color(500,200,0))
+                .setMaterial(new Material()
+                        .setKd(0.5)
+                        .setKs(0.5)
+                        .setnShininess(300));
+//                        .setKt(new Double3(0))
+//                        .setKr(new Double3(0)));
+        scene1.geometries.add(sky);
+
+        //SUN
+        Geometry sun=new Sphere(new Point(0,10,-80),85)
+                .setEmission(new Color(200,80,0))
+                .setMaterial(new Material()
+                        .setKd(0.0)
+                        .setKs(1.0)
+                        .setnShininess(300)
+                        .setKt(new Double3(1.5))
+                        .setKr(new Double3(0.4)));
+        scene1.geometries.add(sun);
+
+        //SEA
+         Geometry sea=new Plane(new Point(0,-15,2),new Vector(0,1,2))
+                .setEmission(new Color(0,200,400))
+                .setMaterial(new Material()
+                        .setKd(0.0)
+                        .setKs(0.5)
+                        .setnShininess(300)
+                        .setKt(new Double3(0.06))
+                        .setKr(new Double3(0.2)));
+
+        scene1.geometries.add(sea);
+
+        //BIRDS
+        Geometry bird1left= new Triangle(new Point(20,25,2),new Point(20,30,2),new Point(1,40,2))
+                .setEmission(new Color(java.awt.Color.black));//left wing
+        Geometry bird1right= new Triangle(new Point(20,25,2),new Point(20,30,2),new Point(35,37,2))
+                .setEmission(new Color(java.awt.Color.black));//right wing
+
+        Geometry bird2left= new Triangle(new Point(35,43.75,0),new Point(35,48,0),new Point(15,62,0))
+                .setEmission(new Color(java.awt.Color.black));//left wing
+        Geometry bird2right= new Triangle(new Point(35,43.75,0),new Point(35,48,0),new Point(55,57.35,0))
+                .setEmission(new Color(java.awt.Color.black));//right wing
+
+        Geometry bird3left= new Triangle(new Point(48,18,-2),new Point(48,23.8,-2),new Point(35,30,0))
+                .setEmission(new Color(java.awt.Color.black));//left wing
+        Geometry bird3right= new Triangle(new Point(35,18,-2),new Point(35,23.8,-2),new Point(52,30,0))
+                .setEmission(new Color(java.awt.Color.black));//right wing
+
+
+        scene1.geometries.add(bird1left,bird1right,bird2left,bird2right,bird3left,bird3right);
+
+
+
+        //light addition
+        scene1.getLights().add( //the sunlight
+                new SpotLight(new Color(800, 400, 400), new Point(0, 30, 50), new Vector(0, -3, -2)) //
+                        .setkL(4E-4).setkQ(2E-5));
+        scene1.getLights().add( //the daylight
+                new SpotLight(new Color(0, 0, 800), new Point(150, -30, 50), new Vector(0, 3, -2)) //
+                        .setkL(4E-4).setkQ(2E-5));
+        scene1.getLights().add( //gave the sea some color
+                new SpotLight(new Color(java.awt.Color.cyan),new Point(0,-10,20),new Vector(0,3,-2)));
+//       scene1.getLights().add( //
+//                new SpotLight(new Color(java.awt.Color.black),new Point(75,0,30),new Vector(5,-60,300)));
+//        scene1.getLights().add( //
+//                new DirectionalLight(new Color(java.awt.Color.black),new Vector(-1986,-500,-200)));
+
+        //creating the picture
+        ImageWriter imageWriter = new ImageWriter("testTheXYZ", 500, 500);
+        camera1.setImageWriter(imageWriter) //
+                .setRayTracer(new RayTracerBasic(scene1)) //
+                .renderImage(); //
+        camera1.writeToImage(); //
+    }
+}
